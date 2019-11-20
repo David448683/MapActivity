a8f382a5739e926c551ea98849630f539b795ad6
Zhandong Qiu <i1710766@nzse.ac.nz>
2019-11-21 09:06:24 +1300

    Remove the navigation and jump to the map straight

--- MapActivity/HomeActivity.cs
+++ MapActivity/HomeActivity.cs
@@ -16,6 +16,10 @@ namespace MapActivity
     public class HomeActivity : Activity
         {
         TextView username;
+        Button btnMap;
+        Button btnList;
+        Button btnBluetooth;
+        Button btnMessage;
         protected override void OnCreate(Bundle savedInstanceState)
             {
             base.OnCreate(savedInstanceState);
@@ -24,6 +28,28 @@ namespace MapActivity
             // Create your application here
             username = FindViewById<TextView>(Resource.Id.lblWelcome);
             username.Text = Intent.GetStringExtra("AccName");
+
+            btnMap = FindViewById<Button>(Resource.Id.btnMap);
+            btnList = FindViewById<Button>(Resource.Id.btnList);
+            btnBluetooth = FindViewById<Button>(Resource.Id.btnBluetooth);
+            btnMessage = FindViewById<Button>(Resource.Id.btnMessage);
+
+            btnMap.Click += (Sender, e) =>
+            {
+                Intent MainActivityIntent = new Intent(this, typeof(MainActivity));
+                StartActivity(MainActivityIntent);
+            };
+
+            btnList.Click += (Sender, e) =>
+            {
+                Intent PlayQuoteActivityIntent = new Intent(this, typeof(PlayQuoteActivity));
+                StartActivity(PlayQuoteActivityIntent);
+            };
+
+          
+
+
+
             }
         }
     }
---
+++ MapActivity/House.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MapActivity
+    {
+    class House
+        {
+        public long Id { get; set; }
+
+        public double BendRoomNo { get; set; }
+
+        public double BathRoomNo { get; set; }
+
+        public string HouseAddress { get; set; }
+
+        public double RentPerWeek { get; set; }
+        }
+    }
