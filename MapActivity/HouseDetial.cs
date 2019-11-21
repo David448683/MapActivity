c89fd7d8e72fa91828db2646396c70f64a494607
Shirui Liu <i1811398@nzse.ac.nz>
2019-11-21 15:38:44 +1300

    i do the fragment for us

---
+++ MapActivity/HouseDetial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MapActivity
+{
+    class HouseDetial
+    {
+        public static string[] Titles = {
+                                      "Property1",
+                                      "Property2",
+                                      "Property3",
+                                      "Property4",
+                                      "Property5",
+                                      "Property6"
+                                                   };
+        public static string[] Dialogue = {
+            " Price for rental: 500$ per week , Tenancy term avaliable : max temancy term 2, full furnished, bills not included but includes cold water, weekly payment "
+
+        };
+    }
+}
