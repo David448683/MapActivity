77e0ed21e6e723cc1ecd4e102270877ddd720722
Shirui Liu <i1811398@nzse.ac.nz>
2019-11-21 16:10:32 +1300

    add the ASP thing to get username and password

--- MapActivity/LoginActivity.cs
+++ MapActivity/LoginActivity.cs
@@ -31,9 +31,37 @@ namespace MapActivity
                 Intent RegisterActivityIntent = new Intent(this, typeof(RegisterActivity));
              
                 StartActivity(RegisterActivityIntent);
+
             };
 
 
+
+            btnLogin.Click += async delegate
+            {
+                var request = HttpWebRequest.Create(string.Format(@"https://10.0.2.2:5001/api/Users"));
+                //http://172.31.99.148:5000/api/Users
+                request.ContentType = "application/json";
+                request.Method = "GET";
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    if (response.StatusCode != HttpStatusCode.OK)
+                        Console.Out.WriteLine("Error fetching data. Server returned status code: {0}", response.StatusCode);
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        var content = reader.ReadToEnd();
+                        if (string.IsNullOrWhiteSpace(content))
+                        {
+                            Console.Out.WriteLine("Response contained empty body...");
+                        }
+                        else
+                        {
+                            var userList = JsonConvert.DeserializeObject<List<User>>(content);
+                            Console.Out.WriteLine("Response Body: \r\n {0}", content);
+
+                        }
+                    }
+                }
+            };
         }
 
         private T FindViewById<T>(object edfirstname)
